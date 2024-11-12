e2a70f160b412c84a647fdb34ba0428ea0825b02
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-11-12 13:51:27 -0300

    alterado para um jeito melhor de definir busca por hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,19 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
-      -- vim.keymap.set('n', '<C-p>', builtin.find_files({hidden=true}), { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<C-p>', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }) 
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+      require('telescope').setup{
+        defaults = { },
+        pickers = {
+          find_files = {
+            hidden = true,
+          }
+        },
+        extensions = { }
+      }
     end
 }
