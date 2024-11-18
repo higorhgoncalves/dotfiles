231900cc46d2f54ff8368f5ff97384f374828647
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2024-11-18 01:47:35 -0300

    adicionado dashboard adicionado none-ls

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -11,8 +11,8 @@ return {
             require("mason-lspconfig").setup {
                 ensure_installed = { 
                   "lua_ls", 
-                  --"intelephense", 
-                  --"ts_ls" 
+                  "intelephense", 
+                  "ts_ls" 
                 },
             }
         end
@@ -22,8 +22,8 @@ return {
         config = function()
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup {}
-            --lspconfig.intelephense.setup {}
-            --lspconfig.ts_ls.setup {}
+            lspconfig.intelephense.setup {}
+            lspconfig.ts_ls.setup {}
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
       local configs = require("nvim-treesitter.configs")                                                                                             
                                                                                                                                                      
       configs.setup({                                                                                                                                
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },                                       
+          -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },                                       
+          auto_install = true,
           highlight = { enable = true },                                                                                                             
           indent = { enable = true },                                                                                                                
       })                                                                                                                                           
