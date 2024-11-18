def6f757abc1b39a14ec6c908c230a94dfaf9db3
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-11-18 17:21:16 -0300

    erros indicados pelo editor

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
       local builtin = require('telescope.builtin')
       -- vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' }) 
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
       vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "[F]ind [O]ldfiles" })
@@ -32,7 +32,6 @@ return {
           }
         }
       }
-      
       require("telescope").load_extension("ui-select")
     end
   }
