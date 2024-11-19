c2a8e5caa89e2669185f6a3fd48f6d575960f3ae
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-11-19 08:20:40 -0300

    dashboard finalizado

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -32,59 +32,63 @@ return {
             config = {
                 header = vim.split(logo, "\n"),
                 -- stylua: ignore
-                center = {{
-                    action = 'lua LazyVim.pick()()',
-                    desc = " Find File",
-                    icon = " ",
-                    key = "f"
-                }, {
-                    action = "ene | startinsert",
-                    desc = " New File",
-                    icon = " ",
-                    key = "n"
-                }, {
-                    action = 'lua LazyVim.pick("oldfiles")()',
-                    desc = " Recent Files",
-                    icon = " ",
-                    key = "r"
-                }, {
-                    action = 'lua LazyVim.pick("live_grep")()',
-                    desc = " Find Text",
-                    icon = " ",
-                    key = "g"
-                }, {
-                    action = 'lua LazyVim.pick.config_files()()',
-                    desc = " Config",
-                    icon = " ",
-                    key = "c"
-                }, {
-                    action = 'lua require("persistence").load()',
-                    desc = " Restore Session",
-                    icon = " ",
-                    key = "s"
-                }, {
-                    action = "LazyExtras",
-                    desc = " Lazy Extras",
-                    icon = " ",
-                    key = "x"
-                }, {
-                    action = "Lazy",
-                    desc = " Lazy",
-                    icon = "󰒲 ",
-                    key = "l"
-                }, {
-                    action = function()
-                        vim.api.nvim_input("<cmd>qa<cr>")
-                    end,
-                    desc = " Quit",
-                    icon = " ",
-                    key = "q"
-                }},
-                footer = function()
-                    local stats = require("lazy").stats()
-                    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                    return {"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"}
-                end
+                center = {
+                    {
+                        action = "Neotree toggle",
+                        desc = " Browse Files",
+                        icon = " ",
+                        key = "b"
+                    },
+                    {
+                        action = 'Telescope find_files',
+                        desc = " Find File",
+                        icon = " ",
+                        key = "ff"
+                    },
+                    {
+                        action = 'Telescope oldfiles',
+                        desc = " Recent Files",
+                        icon = " ",
+                        key = "fo"
+                    },
+                    {
+                        action = 'Telescope live_grep',
+                        desc = " Find Text",
+                        icon = " ",
+                        key = "fg"
+                    },
+                    {
+                        action = "Lazy sync",
+                        desc = " Update plugins",
+                        icon = "󰊳 ",
+                        key = "u"
+                    },
+                    {
+                        action = "Lazy",
+                        desc = " Lazy",
+                        icon = "󰒲 ",
+                        key = "l"
+                    },
+                    {
+                        action = "Mason",
+                        desc = " Install language tools",
+                        icon = " ",
+                        key = "l"
+                    },
+                    {
+                        action = function()
+                            vim.api.nvim_input("<cmd>qa<cr>")
+                        end,
+                        desc = " Quit",
+                        icon = " ",
+                        key = "q"
+                    }
+                },
+                -- footer = function()
+                --     local stats = require("lazy").stats()
+                --     local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                --     return {"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms"}
+                -- end
             }
         }
 
