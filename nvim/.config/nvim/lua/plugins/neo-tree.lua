5f219fbf473872342daf64594d91e4d43413fe5e
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2024-11-27 00:22:13 -0300

    setado keymap igual vscode

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,23 +1,23 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    cmd = 'Neotree',
-    keys = {
-      { '<leader>nt', ':Neotree toggle<CR>', desc = '[N]eoTree [T]oggle', silent = true },
-    },
-    opts = {
-      filesystem = {
-        window = {
-          mappings = {
-            ['\\'] = {'close_window', nowait = true},
-          },
+  "nvim-neo-tree/neo-tree.nvim",
+  -- branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<C-b>', ':Neotree toggle<CR>', desc = '[B]rowse', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['<C-b>'] = {'close_window'},
         },
       },
     },
+  },
 }
