3a2114bfd706014c0d7157f273c645ead9384062
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-11-28 17:54:20 -0300

    adicionado github copilot

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,56 +1,72 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      -- Set up nvim-cmp.
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load({
-        include = { "html", "php" }
-      })
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"github/copilot.vim",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			-- Set up nvim-cmp.
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load({
+				include = { "html", "php" },
+			})
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            local luasnip = require('luasnip')
-            luasnip.lsp_expand(args.body)
-            luasnip.filetype_extend("php", { "html" })
-            luasnip.filetype_extend("html", { "php" })
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'luasnip' },
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						local luasnip = require("luasnip")
+						luasnip.lsp_expand(args.body)
+						luasnip.filetype_extend("php", { "html" })
+						luasnip.filetype_extend("html", { "php" })
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" },
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
 
-      local luasnip = require("luasnip")
+			local luasnip = require("luasnip")
 
-      -- Permitir que snippets de HTML sejam usados em arquivos PHP e vice-versa
-      luasnip.filetype_extend("php", { "html" })
-      luasnip.filetype_extend("html", { "php" })
-    end,
-  },
+			-- Permitir que snippets de HTML sejam usados em arquivos PHP e vice-versa
+			luasnip.filetype_extend("php", { "html" })
+			luasnip.filetype_extend("html", { "php" })
+		end,
+	},
 }
