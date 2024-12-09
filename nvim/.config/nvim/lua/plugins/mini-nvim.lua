4f911a93893e463520a0d77b60a9e8254d6f358c
Higor Henrique Prata Gonçalves <higor.henrique@legisweb.com.br>
2024-12-09 13:15:37 -0300

    removido animation

--- nvim/.config/nvim/lua/plugins/mini-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -17,6 +17,7 @@ return {
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
 
+<<<<<<< Updated upstream
 		require("mini.animate").setup({})
 		require("mini.files").setup()
 
@@ -37,6 +38,25 @@ return {
 			return "%2l:%-2v"
 		end
 
+=======
+		-- require('mini.animate').setup({})
+
+		-- Simple and easy statusline.
+		--  You could remove this setup call if you don't like it,
+		--  and try some other statusline plugin
+		local statusline = require("mini.statusline")
+		-- set use_icons to true if you have a Nerd Font
+		statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+		-- You can configure sections in the statusline by overriding their
+		-- default behavior. For example, here we set the section for
+		-- cursor location to LINE:COLUMN
+		---@diagnostic disable-next-line: duplicate-set-field
+		statusline.section_location = function()
+			return "%2l:%-2v"
+		end
+
+>>>>>>> Stashed changes
 		-- ... and there is more!
 		--  Check out: https://github.com/echasnovski/mini.nvim
 	end,
