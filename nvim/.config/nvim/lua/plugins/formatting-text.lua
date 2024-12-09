32d82fdee34be35c87eaf5551f6491e3ab7775e7
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-09 07:45:00 -0300

    adicionado plugin comentario

--- nvim/.config/nvim/lua/plugins/formatting-text.lua
+++ nvim/.config/nvim/lua/plugins/formatting-text.lua
@@ -1,12 +1,18 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-  {
-    'mg979/vim-visual-multi',
-  }
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+	},
+	{
+		"mg979/vim-visual-multi",
+	},
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
 }
