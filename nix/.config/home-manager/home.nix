5db229d1bde58c914be7520198a9c99f91bcbddc
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-11 18:00:33 -0300

    configuracoes atualizadas

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   # Home Manager precisa de algumas informações sobre você e os caminhos que deve gerenciar.
   home.username = "administrador";
   home.homeDirectory = "/home/administrador";
@@ -11,6 +10,9 @@
   # certifique-se de verificar as notas de lançamento do Home Manager.
   home.stateVersion = "24.05"; # Leia o comentário antes de mudar.
 
+  # Deixe o Home Manager instalar e gerenciar a si mesmo.
+  programs.home-manager.enable = true;
+
   # Definindo um overlay para substituir a versão do openvpn
   nixpkgs.overlays = [
     (final: prev: {
@@ -19,8 +21,10 @@
         version = "2.5.8"; # Versão desejada
 
         src = prev.fetchurl {
-          url = "https://swupdate.openvpn.org/community/releases/openvpn-${version}.tar.gz";
-          sha256 = "a6f315b7231d44527e65901ff646f87d7f07862c87f33531daa109fb48c53db2"; # Substitua pelo hash correto
+          url =
+            "https://swupdate.openvpn.org/community/releases/openvpn-${version}.tar.gz";
+          sha256 =
+            "a6f315b7231d44527e65901ff646f87d7f07862c87f33531daa109fb48c53db2"; # Substitua pelo hash correto
         };
       });
     })
@@ -29,8 +33,8 @@
   # Pacotes a serem instalados
   home.packages = with pkgs; [
     # fontes
-    nerd-fonts.profont
-    nerd-fonts.caskaydia-mono
+    # nerd-fonts.profont
+    # nerd-fonts.caskaydia-mono
     nerd-fonts.jetbrains-mono
 
     # Pacotes de sistema
@@ -47,16 +51,28 @@
     git
     starship
     openvpn
-    
+    bcg729
+
     # Pacotes de desenvolvimento
     neovim
     cargo
     lua51Packages.luarocks
     nodejs
     ripgrep
-    nil
-    nixpkgs-fmt
-    
+    nixd
+    nixfmt-classic
+
+    # Pacotes VSCode
+    # vscode
+    # vscode-extensions.catppuccin.catppuccin-vsc
+    # vscode-extensions.catppuccin.catppuccin-vsc-icons
+    # vscode-extensions.devsense.phptools-vscode
+    # vscode-extensions.xdebug.php-debug
+    # vscode-extensions.github.copilot
+    # vscode-extensions.github.copilot-chat
+    # vscode-extensions.eamodio.gitlens
+    # vscode-extensions.ecmel.vscode-html-css
+
     # Aplicativos gráficos
     pidgin
 
@@ -71,7 +87,37 @@
 
   # Home Manager é muito bom em gerenciar arquivos de configuração. A principal maneira de gerenciar
   # arquivos simples é através de 'home.file'.
-  home.file = {};
+  home.file = {
+    # ".config/kitty/kitty.con" = {
+    #   text = ''
+    #     # Shell Styling
+    #     shell_integration no-cursor
+    #     cursor_shape underline
+
+    #     # Window Style
+    #     # wayland_titlebar_color background
+    #     hide_window_decorations yes
+
+    #     # Hyprland :)
+    #     linux_display_server wayland
+
+    #     background_opacity 0.75
+    #     adjust_line_height 120%
+
+    #     # BEGIN_KITTY_FONTS
+    #     font_family      family="JetBrainsMonoNL Nerd Font"
+    #     bold_font        auto
+    #     italic_font      auto
+    #     bold_italic_font auto
+    #     # END_KITTY_FONTS
+
+    #     # BEGIN_KITTY_THEME
+    #     # Catppuccin-Mocha
+    #     include current-theme.conf
+    #     # END_KITTY_THEME
+    #   '';
+    # };
+  };
 
   # Home Manager também pode gerenciar suas variáveis de ambiente através de 'home.sessionVariables'.
   # Se você não quiser gerenciar seu shell através do Home Manager, então você deve manualmente
@@ -88,9 +134,6 @@
     QT_QPA_PLATFORMTHEME = "qt5ct";
     QT_STYLE_OVERRIDE = "Fusion";
   };
-
-  # Deixe o Home Manager instalar e gerenciar a si mesmo.
-  programs.home-manager.enable = true;
 
   # Configurações do Git
   programs.git = {
@@ -111,46 +154,46 @@
     '';
 
     gtk3.extraConfig = {
-      gtk-theme-name="catppuccin-mocha-blue-standard+default";
+      gtk-theme-name = "catppuccin-mocha-blue-standard+default";
     };
 
     gtk4.extraConfig = {
-      gtk-theme-name="catppuccin-mocha-blue-standard+default";
+      gtk-theme-name = "catppuccin-mocha-blue-standard+default";
     };
   };
 
   # Configurações do Fish shell
   programs.fish = {
     enable = true;
-    
+
     interactiveShellInit = ''
       if status is-interactive
-        set -U fish_greeting ""
+      set -U fish_greeting ""
 
-        starship init fish | source
+      starship init fish | source
 
-        set -gx ATUIN_NOBIND "true"
-        atuin init fish | source
+      set -gx ATUIN_NOBIND "true"
+      atuin init fish | source
 
-        # vincular ao ctrl-r no modo normal e de inserção, adicione quaisquer outras vinculações aqui também
-        bind \cr _atuin_search
-        bind -M insert \cr _atuin_search
-        
-        alias ls="eza"
-        alias cd="z"
+      # vincular ao ctrl-r no modo normal e de inserção, adicione quaisquer outras vinculações aqui também
+      bind \cr _atuin_search
+      bind -M insert \cr _atuin_search
 
-        [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+      alias ls="eza"
+      alias cd="z"
 
-        set -x PATH $HOME/.nix-profile/bin $HOME/.nix-profile/sbin $PATH
-        thefuck --alias | source
-        zoxide init fish | source
+      [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+      set -x PATH $HOME/.nix-profile/bin $HOME/.nix-profile/sbin $PATH
+      thefuck --alias | source
+      zoxide init fish | source
       end
     '';
   };
 
   # Habilitar XDG
-  xdg.enable = true;
-  targets.genericLinux.enable = true;
+  # xdg.enable = true;
+  # targets.genericLinux.enable = true;
 
   # Configurações de aplicativos padrão
   xdg.mimeApps.defaultApplications = {
