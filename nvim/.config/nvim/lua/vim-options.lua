3de96a520e0a601b65db6f42239c2249fee52c03
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-12 18:00:33 -0300

    adicionado nowrap; comentado clipboard;

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set autoindent")
+vim.cmd("set nowrap")
 vim.cmd("inoremap <S-Tab> <C-d>")
 
 vim.cmd("set number")
@@ -10,22 +11,22 @@ vim.cmd("set number")
 -- vim.o.statuscolumn = "%l\\ %r"
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
-vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    },
-}
+-- vim.g.clipboard = {
+-- 	name = "OSC 52",
+-- 	copy = {
+-- 		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+-- 		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+-- 	},
+-- 	paste = {
+-- 		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+-- 		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+-- 	},
+-- }
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { desc = 'Navigate to the left pane' })
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', { desc = 'Navigate to the bottom pane' })
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { desc = 'Navigate to the top pane' })
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { desc = 'Navigate to the right pane' })
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate to the left pane" })
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate to the bottom pane" })
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Navigate to the top pane" })
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate to the right pane" })
 
-vim.keymap.set('n', '<M-r>', ':set rnu! rnu?<CR>', { desc = 'Toggle relative line numbers' })
+vim.keymap.set("n", "<M-r>", ":set rnu! rnu?<CR>", { desc = "Toggle relative line numbers" })
