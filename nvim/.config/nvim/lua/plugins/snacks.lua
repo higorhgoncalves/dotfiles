4adcb2df502bdd5d927b3d3edc1d278e51c0badf
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-12 18:00:45 -0300

    inicio de migracao snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,9 +7,59 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		-- bigfile = { enabled = true },
-		-- notifier = { enabled = true },
+		notifier = { enabled = true, timeout = 3000, style = "fancy" },
+		input = {
+			enabled = true,
+			backdrop = false,
+			position = "float",
+			border = "rounded",
+			title_pos = "center",
+			height = 1,
+			width = 60,
+			relative = "editor",
+			row = 2,
+			-- relative = "cursor",
+			-- row = -3,
+			-- col = 0,
+			wo = {
+				winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+				cursorline = false,
+			},
+			bo = { filetype = "snacks_input" },
+			keys = {
+				i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i" },
+				-- i_esc = { "<esc>", "stopinsert", mode = "i" },
+				i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i" },
+				i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i" },
+				q = "cancel",
+			},
+		},
 		-- quickfile = { enabled = true },
 		-- statuscolumn = { enabled = true },
 		-- words = { enabled = true },
+		lazygit = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit Current File History",
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit Log (cwd)",
+		},
 	},
 }
