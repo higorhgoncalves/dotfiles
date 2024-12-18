0ba153dedd6e9bacb140df66cb02a6cae9abe5e8
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-18 17:48:55 -0300

    melhorias no formatter

--- nvim/.config/nvim/lua/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -36,8 +36,10 @@ return { -- Autoformat
 			-- python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-            php = { "phpcbf", "php-cs-fixer" },
+			html = { "prettier", "prettierd", stop_after_first = true },
+			css = { "prettier", "prettierd", stop_after_first = true },
+			javascript = { "prettier", "prettierd", stop_after_first = true },
+			php = { "pretty-php", "pint", "php-cs-fixer", stop_after_first = true },
 		},
 	},
 }
