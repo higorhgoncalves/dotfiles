3d6dea70cce2e4f6563ba8effb5ee72aae82f7e7
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-19 16:06:36 -0300

    configurado listas de busca

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,81 +1,116 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        -- tag = '0.1.8',
-        dependencies = {
-            { 'nvim-lua/plenary.nvim' },
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        },
-        keys = {
-            { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find find files' },
-            { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Find live grep' },
-            { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
-            { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Find help tags' },
-            { '<leader>fo', '<cmd>Telescope oldfiles<cr>', desc = "Find oldfiles" },
-            { '<leader>fn', '<cmd>Telescope notify<cr>', desc = "Find notifications" },
-            { '<leader>fr', '<cmd>Telescope lsp_references<cr>', desc = "Find references" },
-            { '<leader>/', '<cmd>Telescope current_buffer_fuzzy_find<cr>', desc = 'Find in buffer' },
-        },
-        config = function()
-            local telescope = require('telescope')
-            telescope.setup {
-                defaults = {
-                    vimgrep_arguments = {
-                        'rg',
-                        '--color=never',
-                        '--no-heading',
-                        '--with-filename',
-                        '--line-number',
-                        '--column',
-                        '--smart-case',
-                        '--hidden',
-                    },
-                },
-                pickers = {
-                    find_files = {
-                        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
-                        additional_args = function(_)
-                            return { "--hidden" }
-                        end
-                    },
-                    live_grep = {
-                        file_ignore_patterns = { 'node_modules', '.git', '.venv' },
-                        hidden = true,
-                    },
-                },
-                extensions = {
-                    "fzf",
-                }
-            }
+	{
+		"nvim-telescope/telescope.nvim",
+		-- tag = '0.1.8',
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		keys = {
+			{ "<leader>f/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find in buffer" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find find files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find live grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+			{ "<leader>fB", "<cmd>Telescope builtin<cr>", desc = "Find built-in pickers" },
+			{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Find keymaps" },
+			{ "<leader>fC", "<cmd>Telescope commands<cr>", desc = "Find available plugin/user commands" },
+			{ "<leader>f:", "<cmd>Telescope command_history<cr>", desc = "Find commands that were executed recently" },
+			{ "<leader>fq", "<cmd>Telescope quickfix<cr>", desc = "Find in quickfix list" },
+			{ "<leader>fM", "<cmd>Telescope man_pages<cr>", desc = "Find in manpage entries" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find help tags" },
+			{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Find in previously open files" },
+			{ "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Find notifications" },
+			{ "<leader>fD", "<cmd>Telescope diagnostics<cr>", desc = "Find LSP diagnostics" },
+			{ "<leader>fI", "<cmd>Telescope lsp_incoming_calls<cr>", desc = "Find LSP incoming calls" },
+			{ "<leader>fO", "<cmd>Telescope lsp_outgoing_calls<cr>", desc = "Find LSP outgoing calls" },
+			{ "<leader>fd", "<cmd>Telescope lsp_definitions<cr>", desc = "Find LSP definitions" },
+			{ "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "Find LSP references" },
+			{ "<leader>fi", "<cmd>Telescope lsp_implementations<cr>", desc = "Find LSP implementations" },
+			{ "<leader>ft", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Find LSP type definitions" },
+			{ "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Find LSP document symbols" },
+			{ "<leader>fw", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Find LSP workspace symbols" },
+			{
+				"<leader>fW",
+				"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+				desc = "Find LSP dynamic workspace symbols",
+			},
+			{ "<leader>fGc", "<cmd>Telescope git_commits<cr>", desc = "Find Git Commits" },
+			{ "<leader>fGC", "<cmd>Telescope git_bcommits<cr>", desc = "Find Git Buffer's Commits" },
+			{ "<leader>fGb", "<cmd>Telescope git_branches<cr>", desc = "Find Git Branches" },
+			{ "<leader>fGs", "<cmd>Telescope git_status<cr>", desc = "Find Git Status" },
+			{ "<leader>fGf", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
+			{ "<leader>fGa", "<cmd>Telescope git_stash<cr>", desc = "Find Git Stash" },
+			{
+				"<leader>fc",
+				'<cmd>lua require("telescope.builtin").find_files { cwd = vim.fn.stdpath("config") }<cr>',
+				desc = "Find in config files",
+			},
+			{
+				"<leader>fp",
+				'<cmd>lua require("telescope.builtin").find_files { cwd = vim.fn.stdpath("data") }<cr>',
+				desc = "Find in packages",
+			},
+		},
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+					},
+				},
+				pickers = {
+					find_files = {
+						file_ignore_patterns = { "node_modules", ".git", ".venv" },
+						additional_args = function(_)
+							return { "--hidden" }
+						end,
+					},
+					live_grep = {
+						file_ignore_patterns = { "node_modules", ".git", ".venv" },
+						hidden = true,
+					},
+				},
+				extensions = {
+					"fzf",
+				},
+			})
 
-            telescope.load_extension('fzf')
+			telescope.load_extension("fzf")
 
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>fc', function () builtin.find_files { cwd = vim.fn.stdpath("config") } end, { desc = 'Find in config' })
-            vim.keymap.set('n', '<leader>fp', function () builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") } end, { desc = 'Find in packages' })
-            require "config.telescope.multigrep".setup()
+			-- local builtin = require('telescope.builtin')
+			-- vim.keymap.set('n', '<leader>fc', function () builtin.find_files { cwd = vim.fn.stdpath("config") } end, { desc = 'Find in config' })
+			-- vim.keymap.set('n', '<leader>fp', function () builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") } end, { desc = 'Find in packages' })
+			require("config.telescope.multigrep").setup()
 
-            -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-            -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-            -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-            -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-            -- vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope oldfiles" })
-            -- vim.keymap.set('n', '<leader>fn', ":Telescope notify<CR>", { desc = "Telescope notifications" })
-            -- vim.keymap.set("n", '<leader>gr', builtin.lsp_references, { desc = "Telescope LSP references" })
-            -- vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Telescope in buffer' })
-        end
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup {
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {}
-                    }
-                }
-            }
-            require("telescope").load_extension("ui-select")
-        end
-    }
+			-- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+			-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+			-- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+			-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+			-- vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope oldfiles" })
+			-- vim.keymap.set('n', '<leader>fn', ":Telescope notify<CR>", { desc = "Telescope notifications" })
+			-- vim.keymap.set("n", '<leader>gr', builtin.lsp_references, { desc = "Telescope LSP references" })
+			-- vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Telescope in buffer' })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
