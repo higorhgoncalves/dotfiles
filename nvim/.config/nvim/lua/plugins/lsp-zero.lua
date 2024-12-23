12d973b8031a0ba6289e6ca9b2daef8e670223e9
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2024-12-22 21:37:33 -0300

    alterado luasnip para vim.snippet

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -25,46 +25,28 @@ return {
 			luasnip.filetype_extend("html", { "css", "javascript" })
 
 			cmp.setup({
+				-- snippet = {
+				-- 	expand = function(args)
+				-- 		luasnip.lsp_expand(args.body)
+				-- 	end,
+				-- },
 				snippet = {
 					expand = function(args)
-						luasnip.lsp_expand(args.body)
+						-- You need Neovim v0.10 to use vim.snippet
+						vim.snippet.expand(args.body)
 					end,
 				},
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
+				-- mapping = cmp.mapping.preset.insert({}),
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					-- -- Super tab
-					-- ["<Tab>"] = cmp.mapping(function(fallback)
-					-- 	local col = vim.fn.col(".") - 1
-					--
-					-- 	if cmp.visible() then
-					-- 		cmp.select_next_item({ behavior = "select" })
-					-- 	elseif luasnip.expand_or_locally_jumpable() then
-					-- 		luasnip.expand_or_jump()
-					-- 	elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-					-- 		fallback()
-					-- 	else
-					-- 		cmp.complete()
-					-- 	end
-					-- end, { "i", "s" }),
-					--
-					-- -- Super shift tab
-					-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-					-- 	if cmp.visible() then
-					-- 		cmp.select_prev_item({ behavior = "select" })
-					-- 	elseif luasnip.locally_jumpable(-1) then
-					-- 		luasnip.jump(-1)
-					-- 	else
-					-- 		fallback()
-					-- 	end
-					-- end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "luasnip" },
