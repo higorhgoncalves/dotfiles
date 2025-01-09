7b160571b53d7d651da20f32595a09bd753cda94
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-08 21:47:18 -0300

    corrigido blink cmp 0.10

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,4 @@
+-- NOTE: Specify the trigger character(s) used for luasnip
 return {
 	{
 		"saghen/blink.cmp",
@@ -15,17 +16,6 @@ return {
 				opts = {},
 				version = not vim.g.lazyvim_blink_main and "*",
 			},
-			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				opts = {
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
 		},
 
 		-- use a release tag to download pre-built binaries
@@ -35,17 +25,14 @@ return {
 		-- If you use nix, you can build from source using latest nightly rust with:
 		-- build = 'nix run .#build-plugin',
 
-		---@module 'blink.cmp'
-		---@diagnostic disable-next-line: undefined-doc-name
-		---@type blink.cmp.Config
-		opts = {
+		opts = function(_, opts)
 			-- 'default' for mappings similar to built-in completion
 			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
 			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = "default" },
+			opts.keymap = { preset = "default" }
 
-			appearance = {
+			opts.appearance = {
 				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
 				-- Useful for when your theme doesn't support blink.cmp
 				-- Will be removed in a future release
@@ -56,16 +43,16 @@ return {
 				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
 				-- Adjusts spacing to ensure icons are aligned
 				-- nerd_font_variant = "mono",
-			},
+			}
 
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			sources = {
+			opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
 				default = function(ctx)
 					local _ = ctx
 					local success, node = pcall(vim.treesitter.get_node)
 					if vim.bo.filetype == "lua" then
-						return { "lazydev", "lsp", "path", "snippets", "buffer" }
+						return { "lsp", "path", "snippets", "buffer" }
 					elseif
 						success
 						and node
@@ -73,19 +60,16 @@ return {
 					then
 						return { "buffer" }
 					else
-						return { "lazydev", "lsp", "path", "snippets", "buffer", "dadbod" }
+						return { "lsp", "path", "snippets", "buffer", "dadbod" }
 					end
 				end,
 				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100, -- show at a higher priority than lsp
-					},
 					lsp = {
 						name = "lsp",
+						enabled = true,
 						module = "blink.cmp.sources.lsp",
-						fallbacks = { "buffer" },
+						-- kind = "LSP",
+						fallbacks = { "snippets", "buffer" },
 						-- Filter text items from the LSP provider, since we have the buffer provider for that
 						transform_items = function(_, items)
 							for _, item in ipairs(items) do
@@ -98,31 +82,15 @@ return {
 								return item.kind ~= require("blink.cmp.types").CompletionItemKind.Text
 							end, items)
 						end,
-						-- score_offset = 90,
+						score_offset = 90,
 					},
-					-- luasnip = {
-					-- 	name = "luasnip",
-					-- 	enabled = true,
-					-- 	module = "blink.cmp.sources.luasnip",
-					-- 	min_keyword_length = 2,
-					-- 	fallbacks = { "snippets" },
-					-- 	score_offset = 85,
-					-- 	max_items = 8,
-					-- 	opts = {
-					-- 		-- Whether to use show_condition for filtering snippets
-					-- 		use_show_condition = true,
-					-- 		-- Whether to show autosnippets in the completion list
-					-- 		show_autosnippets = true,
-					-- 	},
-					-- },
 					path = {
 						name = "Path",
 						module = "blink.cmp.sources.path",
-						score_offset = 3,
+						score_offset = 25,
 						-- When typing a path, I would get snippets and text in the
-						-- suggestions, I want those to show only if there are no path
-						-- suggestions
-						fallbacks = { "buffer" },
+						-- suggestions, I want those to show only if there are no path suggestions
+						fallbacks = { "snippets", "buffer" },
 						opts = {
 							trailing_slash = false,
 							label_trailing_slash = true,
@@ -138,14 +106,15 @@ return {
 						max_items = 3,
 						module = "blink.cmp.sources.buffer",
 						min_keyword_length = 4,
+						score_offset = 15,
 					},
 					snippets = {
 						name = "snippets",
 						enabled = true,
-						max_items = 4,
-						module = "blink.cmp.sources.snippets",
+						max_items = 8,
 						min_keyword_length = 2,
-						score_offset = 80,
+						module = "blink.cmp.sources.snippets",
+						score_offset = 85,
 						opts = {
 							friendly_snippets = true,
 							-- search_paths = {
@@ -178,26 +147,26 @@ return {
 					end
 					return {}
 				end,
-			},
+			})
 
-			snippets = {
-				-- expand = function(snippet)
-				-- 	require("luasnip").lsp_expand(snippet)
-				-- end,
-				-- active = function(filter)
-				-- 	if filter and filter.direction then
-				-- 		return require("luasnip").jumpable(filter.direction)
-				-- 	end
-				-- 	return require("luasnip").in_snippet()
-				-- end,
-				-- jump = function(direction)
-				-- 	require("luasnip").jump(direction)
-				-- end,
+			opts.snippets = {
 				preset = "default",
-			},
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			}
 
 			-- Style
-			completion = {
+			opts.completion = {
 				menu = {
 					border = "rounded",
 					draw = {
@@ -233,16 +202,16 @@ return {
 
 				-- Display a preview of the selected item on the current line
 				-- ghost_text = { enabled = true },
-			},
+			}
 
 			-- Experimental signature help support
-			signature = {
+			opts.signature = {
 				enabled = true,
 				window = {
 					border = "rounded",
 				},
-			},
-		},
+			}
+		end,
 		opts_extend = { "sources.default" },
 	},
 }
