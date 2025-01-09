857432d85fa280c781a0b9fd413402eda4758b88
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-09 15:49:14 -0300

    configurado keymap para remover arquivo do harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,19 +9,34 @@ return {
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
 		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
+			local finder = function()
+				local paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(paths, item.value)
+				end
+
+				return require("telescope.finders").new_table({
+					results = paths,
+				})
 			end
 
 			require("telescope.pickers")
 				.new({}, {
 					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
+					finder = finder(),
 					previewer = conf.file_previewer({}),
 					sorter = conf.generic_sorter({}),
+					attach_mappings = function(prompt_bufnr, map)
+						map("i", "<C-d>", function()
+							local state = require("telescope.actions.state")
+							local selected_entry = state.get_selected_entry()
+							local current_picker = state.get_current_picker(prompt_bufnr)
+
+							table.remove(harpoon_files.items, selected_entry.index)
+							current_picker:refresh(finder())
+						end)
+						return true
+					end,
 				})
 				:find()
 		end
@@ -36,9 +51,9 @@ return {
 					harpoon.ui:select_menu_item({ split = true })
 				end, { buffer = cx.bufnr, desc = "Open in horizontal split" })
 
-				-- vim.keymap.set("n", "<C-t>", function()
-				-- 	harpoon.ui:select_menu_item({ tabedit = true })
-				-- end, { buffer = cx.bufnr, desc = "Open in new tab" })
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon.ui:select_menu_item({ tabedit = true })
+				end, { buffer = cx.bufnr, desc = "Open in new tab" })
 			end,
 		})
 
@@ -51,24 +66,24 @@ return {
 
 		vim.keymap.set("n", "<leader>wa", function()
 			harpoon:list():add()
-		end, { desc = 'Add to Workspace Navigation List' })
+		end, { desc = "Add to Workspace Navigation List" })
 		vim.keymap.set("n", "<leader>wr", function()
 			harpoon:list():remove()
-		end, { desc = 'Remove from Workspace Navigation List' })
+		end, { desc = "Remove Workspace Navigation List" })
 		-- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-		-- vim.keymap.set("n", "<C-h>", function()
-		-- 	harpoon:list():select(1)
-		-- end)
-		-- vim.keymap.set("n", "<C-t>", function()
-		-- 	harpoon:list():select(2)
-		-- end)
-		-- vim.keymap.set("n", "<C-n>", function()
-		-- 	harpoon:list():select(3)
-		-- end)
-		-- vim.keymap.set("n", "<C-s>", function()
-		-- 	harpoon:list():select(4)
-		-- end)
+		vim.keymap.set("n", "<C-1>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-2>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-3>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-4>", function()
+			harpoon:list():select(4)
+		end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<M-S-Tab>", function()
