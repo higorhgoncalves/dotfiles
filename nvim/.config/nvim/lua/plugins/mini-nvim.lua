59bdc315787df19e19194ce74eda028e2bc71951
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-20 17:35:00 -0300

    configurado varios plugins

--- nvim/.config/nvim/lua/plugins/mini-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -1,6 +1,37 @@
 return {
 	"echasnovski/mini.nvim",
 	version = false,
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"Trouble",
+				"alpha",
+				"dashboard",
+				"fzf",
+				"help",
+				"lazy",
+				"mason",
+				"neo-tree",
+				"notify",
+				"snacks_dashboard",
+				"snacks_notif",
+				"snacks_terminal",
+				"snacks_win",
+				"toggleterm",
+				"trouble",
+			},
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "SnacksDashboardOpened",
+			callback = function(data)
+				vim.b[data.buf].miniindentscope_disable = true
+			end,
+		})
+	end,
 	config = function()
 		-- Better Around/Inside textobjects
 		--
@@ -16,6 +47,21 @@ return {
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
 		require("mini.surround").setup()
+
+		require("mini.comment").setup()
+		require("mini.move").setup()
+		require("mini.operators").setup()
+		require("mini.icons").setup()
+		require("mini.pairs").setup()
+		require("mini.indentscope").setup({
+			event = "LazyFile",
+			draw = {
+				delay = 50,
+				animation = require("mini.indentscope").gen_animation.none(),
+			},
+			-- symbol = "│",
+			symbol = "▏",
+		})
 
 		-- File browser
 		--
