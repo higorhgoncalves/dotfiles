1c5a141a613d686b1ef130d2dcb76c53b6029be4
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-20 17:33:15 -0300

    ativado nvim test

--- nvim/.config/nvim/lua/plugins/nvim-test.lua
+++ nvim/.config/nvim/lua/plugins/nvim-test.lua
@@ -1,12 +1,12 @@
 return {
-  "vim-test/vim-test",
-  dependencies = {
-    "preservim/vimux",
-  },
-  -- vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', { desc = 'Run test nearest' }),
-  -- vim.keymap.set('n', '<leader>T', ':TestFile<CR>', { desc = 'Run test file' }),
-  -- vim.keymap.set('n', '<leader>a', ':TestSuit<CR>', { desc = 'Run test suit' }),
-  -- vim.keymap.set('n', '<leader>l', ':TestLast<CR>', { desc = 'Run last test' }),
-  -- vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', { desc = 'Visit test file' }),
-  -- vim.cmd("let test#strategy = 'vimux'"),
+	"vim-test/vim-test",
+	dependencies = {
+		"preservim/vimux",
+	},
+	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { desc = "Run test nearest" }),
+	vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { desc = "Run test file" }),
+	vim.keymap.set("n", "<leader>a", ":TestSuit<CR>", { desc = "Run test suit" }),
+	vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { desc = "Run last test" }),
+	vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Visit test file" }),
+	vim.cmd("let test#strategy = 'vimux'"),
 }
