15c7123e832a1be49b3078334bd5d01f4c6b2f3d
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-20 17:31:03 -0300

    alterado comandos para lua

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,16 +1,19 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set autoindent")
-vim.cmd("set smartindent")
-vim.cmd("set nowrap")
+-- Indent Opts
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
 vim.cmd("inoremap <S-Tab> <C-d>")
 
-vim.cmd("set number")
--- vim.cmd("set number relativenumber")
+-- Line Numbers Opts
+vim.opt.number = true
+-- vim.opt.relativenumber = true
 -- vim.o.statuscolumn = "%l\\ %r"
-vim.g.mapleader = " "
+
+-- Clipboard Opts
 vim.opt.clipboard = "unnamedplus"
 -- vim.g.clipboard = {
 -- 	name = "OSC 52",
@@ -29,9 +32,17 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate to the left pan
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate to the bottom pane" })
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Navigate to the top pane" })
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate to the right pane" })
+
+-- Other keymaps
+vim.keymap.set("n", "<M-r>", ":set rnu! rnu?<CR>", { desc = "Toggle relative line numbers" })
 vim.keymap.set("n", "<leader>di", "gg=G", { desc = "Document Auto Indent" })
 vim.keymap.set("n", "<leader>dca", ":let @+ = expand('%:p')<CR>", { desc = "Document Clipboard Absolute Path" })
 vim.keymap.set("n", "<leader>dcr", ":let @+ = expand('%:.')<CR>", { desc = "Document Clipboard Relative Path" })
 vim.keymap.set("n", "<leader>dcf", ":let @+ = expand('%:t')<CR>", { desc = "Document Clipboard Filename" })
 vim.keymap.set("n", "<leader>dcp", ":let @+ = expand('%')<CR>", { desc = "Document Clipboard Path" })
-vim.keymap.set("n", "<M-r>", ":set rnu! rnu?<CR>", { desc = "Toggle relative line numbers" })
+
+-- Folding Opts
+vim.opt.foldenable = true
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
