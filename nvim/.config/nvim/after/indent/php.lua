cc01bc94b44763a43646caf6f238d12a2f897f7d
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-01-30 16:43:54 -0300

    tentativa de correcao indentacao arquivos .php padrao e com html

--- nvim/.config/nvim/after/indent/php.lua
+++ nvim/.config/nvim/after/indent/php.lua
@@ -1,3 +1,5 @@
-vim.o.autoindent = true
-vim.o.smartindent = true
+vim.bo.autoindent = true
+vim.bo.smartindent = true
 -- vim.o.indentexpr = ""
+vim.g.php_indent_closing = 0
+vim.g.php_indent_auto = 1
