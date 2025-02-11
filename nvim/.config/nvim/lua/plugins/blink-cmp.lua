7a7a46f6863dc88880b69d2858e08756c4805a86
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-02-11 15:12:55 -0300

    alterado para utilizar release

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -9,9 +9,9 @@ return {
 		},
 
 		-- use a release tag to download pre-built binaries
-		-- version = "*",
+		version = "*",
 		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		build = "cargo build --release",
+		-- build = "cargo build --release",
 
 		opts = function(_, opts)
 			-- 'default' for mappings similar to built-in completion
