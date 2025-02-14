11701d44085ba486bed3ab6c0622c4b042810f67
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-02-13 22:53:37 -0300

    habilitado blade

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,146 +1,54 @@
+-- add more treesitter parsers
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		keys = {
-			{ "<c-space>", desc = "Increment Selection" },
-			{ "<bs>", desc = "Decrement Selection", mode = "x" },
-		},
-		opts_extend = { "ensure_installed" },
-		-- main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"php",
-				"javascript",
-				"css",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"sql",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				-- additional_vim_regex_highlighting = { "html" },
-			},
-			indent = {
-				enable = true,
-				-- disable = { "html" },
-			},
-			autotag = {
-				enable = true,
-			},
-			-- context_commentstring = {
-			-- 	enable = true,
-			-- 	enable_autocmd = false,
-			-- },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-			textobjects = {
-				move = {
-					enable = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-					},
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-				},
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })
+	end,
+	dependencies = {
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = {
+				custom_calculation = function(_, language_tree)
+					if
+						vim.bo.filetype == "blade"
+						and language_tree._lang ~= "javascript"
+						and language_tree._lang ~= "php"
+					then
+						return "{{-- %s --}}"
+					end
+				end,
 			},
 		},
-		config = function(_, opts)
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-			parser_config.blade = {
-				install_info = {
-					url = "https://github.com/EmranMR/tree-sitter-blade",
-					files = { "src/parser.c" },
-					branch = "main",
-				},
-				filetype = "blade",
-			}
-
-			vim.filetype.add({
-				pattern = {
-					[".*%.blade%.php"] = "blade",
-					[".*%.html"] = "htmldjango",
-					[".*%.html%.jinja"] = "htmldjango",
-					[".*%.html%.jinja2"] = "htmldjango",
-					[".*%.html%.j2"] = "htmldjango",
-				},
-			})
-
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	-- {
-	-- 	"windwp/nvim-ts-autotag",
-	-- 	-- event = "LazyFile",
-	-- 	opts = {},
-	-- },
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-				multiwindow = false, -- Enable multiwindow support.
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-				line_numbers = true,
-				multiline_threshold = 20, -- Maximum number of lines to show for a single context
-				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-				-- Separator between context and content. Should be a single character string, like '-'.
-				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-				separator = nil,
-				zindex = 20, -- The Z-index of the context window
-				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-			})
-		end,
+	opts = {
+		ensure_installed = "all",
+		auto_install = true,
+		highlight = {
+			enable = true,
+		},
+		-- Needed because treesitter highlight turns off autoindent for php files
+		indent = {
+			enable = true,
+		},
 	},
-	-- {
-	-- 	"nvim-treesitter/playground",
-	-- 	cmd = "TSPlaygroundToggle",
-	-- 	config = function()
-	-- 		require("nvim-treesitter.configs").setup({
-	-- 			playground = {
-	-- 				enable = true,
-	-- 				updatetime = 25,
-	-- 				persist_queries = false,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+	config = function(_, opts)
+		---@class ParserInfo[]
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = {
+					"src/parser.c",
+					-- 'src/scanner.cc',
+				},
+				branch = "main",
+				generate_requires_npm = true,
+				requires_generate_from_grammar = true,
+			},
+			filetype = "blade",
+		}
+
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
