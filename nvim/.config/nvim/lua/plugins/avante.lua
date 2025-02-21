7f5119d92975dd8e86639776c84e8fa5ed71e9b2
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-02-20 23:40:29 -0300

    habilitado avante como gerenciador de AI (Copilot)

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,77 @@
+return {
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+        opts = {
+            provider = "copilot",
+            auto_suggestions_provider = "copilot",
+            behaviour = {
+                auto_suggestions = true, -- Experimental stage
+            },
+            mappings = {
+                diff = {
+                    ours = "co",
+                    theirs = "ct",
+                    all_theirs = "ca",
+                    both = "cb",
+                    cursor = "cc",
+                    next = "]x",
+                    prev = "[x",
+                },
+                suggestion = {
+                    accept = "<M-l>",
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
+                },
+                jump = {
+                    next = "]]",
+                    prev = "[[",
+                },
+                submit = {
+                    normal = "<CR>",
+                    insert = "<C-s>",
+                },
+                sidebar = {
+                    apply_all = "A",
+                    apply_cursor = "a",
+                    switch_windows = "<Tab>",
+                    reverse_switch_windows = "<S-Tab>",
+                },
+            },
+            suggestion = {
+                debounce = 600,
+            },
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        dependencies = {
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+            "zbirenbaum/copilot.lua",        -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+        },
+    }
+}
