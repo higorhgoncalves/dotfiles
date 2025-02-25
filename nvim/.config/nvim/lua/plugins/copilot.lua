9d181fc238f09a4ed3d86b8b96b5ae48195624be
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-02-25 14:06:03 -0300

    finalizado config ia
    desabilitado codeium;
    separado copilotchat, provavelmente será removido futuramente;
    desabilitado copilot-lua;
    habilitado copilot;

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,15 +3,16 @@ return {
 		"github/copilot.vim",
 		config = function()
 			vim.g.copilot_no_tab_map = true
-			vim.api.nvim_set_keymap("i", "<C-l>", "copilot#Accept()", { expr = true, silent = true, noremap = false })
 			vim.g.copilot_workspace_folders = {
 				"~/docker-lw/html/legisweb",
 				"~/docker-lw/html/classes",
 			}
+
+			vim.api.nvim_set_keymap("i", "<M-l>", "copilot#Accept()", { expr = true, silent = true, noremap = false })
+			-- <M-l> accept suggestion
 			-- <M-]> next suggestion
 			-- <M-[> previous suggestion
 			-- <C-]> dismiss suggestion
-			-- <C-l> accept suggestion
 			-- <M-Right> accept next word of the current suggestion
 			-- <M-C-Right> accept next line of the next suggestion
 			-- <M-\> open suggestion
