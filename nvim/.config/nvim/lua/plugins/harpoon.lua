4abeaf7fd9469ea14c57ad567ee129c49e015bd5
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-03-14 16:13:28 -0300

    migracao telescope para snacks.picker

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,96 +1,54 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup({})
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "folke/snacks.nvim",
+    },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup({})
 
-		-- basic telescope configuration
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local finder = function()
-				local paths = {}
-				for _, item in ipairs(harpoon_files.items) do
-					table.insert(paths, item.value)
-				end
+        local normalize_list = function(t)
+            local normalized = {}
+            for _, v in pairs(t) do
+                if v ~= nil then
+                    table.insert(normalized, v)
+                end
+            end
+            return normalized
+        end
 
-				return require("telescope.finders").new_table({
-					results = paths,
-				})
-			end
+        local harpoon_snacks = function()
+            Snacks.picker({
+                finder = function()
+                    local file_paths = {}
+                    local list = normalize_list(harpoon:list().items)
+                    for i, item in ipairs(list) do
+                        table.insert(file_paths, { text = item.value, file = item.value })
+                    end
+                    return file_paths
+                end,
+                win = {
+                    input = {
+                        keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+                    },
+                    list = {
+                        keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+                    },
+                },
+                actions = {
+                    harpoon_delete = function(picker, item)
+                        local to_remove = item or picker:selected()
+                        harpoon:list():remove({ value = to_remove.text })
+                        harpoon:list().items = normalize_list(harpoon:list().items)
+                        picker:find({ refresh = true })
+                    end,
+                },
+            })
+        end
 
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = finder(),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-					attach_mappings = function(prompt_bufnr, map)
-						map("i", "<C-d>", function()
-							local state = require("telescope.actions.state")
-							local selected_entry = state.get_selected_entry()
-							local current_picker = state.get_current_picker(prompt_bufnr)
-
-							table.remove(harpoon_files.items, selected_entry.index)
-							current_picker:refresh(finder())
-						end)
-						return true
-					end,
-				})
-				:find()
-		end
-
-		harpoon:extend({
-			UI_CREATE = function(cx)
-				vim.keymap.set("n", "<C-v>", function()
-					harpoon.ui:select_menu_item({ vsplit = true })
-				end, { buffer = cx.bufnr, desc = "Open in vertical split" })
-
-				vim.keymap.set("n", "<C-x>", function()
-					harpoon.ui:select_menu_item({ split = true })
-				end, { buffer = cx.bufnr, desc = "Open in horizontal split" })
-
-				vim.keymap.set("n", "<C-t>", function()
-					harpoon.ui:select_menu_item({ tabedit = true })
-				end, { buffer = cx.bufnr, desc = "Open in new tab" })
-			end,
-		})
-
-		vim.keymap.set("n", "<C-e>", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Navigation List" })
-		vim.keymap.set("n", "<leader>wl", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Open Workspace Navigation List" })
-
-		vim.keymap.set("n", "<leader>wa", function()
-			harpoon:list():add()
-		end, { desc = "Add to Workspace Navigation List" })
-		vim.keymap.set("n", "<leader>wr", function()
-			harpoon:list():remove()
-		end, { desc = "Remove Workspace Navigation List" })
-		-- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-		vim.keymap.set("n", "<C-1>", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<C-2>", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<C-3>", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<C-4>", function()
-			harpoon:list():select(4)
-		end)
-
-		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<M-S-Tab>", function()
-			harpoon:list():prev()
-		end, { desc = "Previous Harpoon buffer" })
-		vim.keymap.set("n", "<M-Tab>", function()
-			harpoon:list():next()
-		end, { desc = "Next Harpoon buffer" })
-	end,
+        vim.keymap.set("n", "<C-e>", function() harpoon_snacks() end, { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>fh", function() harpoon_snacks() end, { noremap = true, silent = true, desc = "Find Harpoon" })
+    end,
 }
