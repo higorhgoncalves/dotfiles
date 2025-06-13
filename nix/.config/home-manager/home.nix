bc5bcf6a435ef5580aa5a336d91bf3990c1d29f5
Higor Henrique <71106509+higorhgoncalves@users.noreply.github.com>
2025-06-13 16:56:46 -0300

    config atualizada home manager

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -164,61 +164,111 @@
   # Configurações do Fish shell
   programs.fish = {
     enable = true;
-
+    shellAliases = {
+      # Definindo aliases para o Fish shell
+      ls = "eza";
+      cd = "z";
+      gnome-control-center =
+        "XDG_CURRENT_DESKTOP=Gnome /usr/bin/gnome-control-center";
+      fzfpass = ''
+        read -s -P "Digite a senha para KeePassXC: " keepasswd; and \
+        printf "%s\n" "$keepasswd" | keepassxc-cli ls "Legisweb.kdbx" | \
+        fzf --multi --reverse --preview="echo {}" --preview-window=down,3,wrap | \
+        xargs -I {} sh -c "printf '%s\n' '$keepasswd' | keepassxc-cli show -q 'Legisweb.kdbx' '{}' -a Password"\
+      '';
+      tmux-restore =
+        "tmux new -d -t temp && tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh && tmux kill-session -t temp && tmux attach";
+      fzftop = ''
+        ps aux | \
+        fzf --multi --reverse --bind 'ctrl-r:reload(ps aux)' --header='Press CTRL-R to reload' --header-lines=2 --preview='echo {}' --preview-window=down,3,wrap | \
+        awk '{print $2}' | \
+        xargs kill -9\
+      '';
+    };
     interactiveShellInit = ''
-        if status is-interactive
-            set -U fish_greeting ""
+      if status is-interactive
+        set -U fish_greeting ""
 
-            starship init fish | source
+        starship init fish | source
 
-            set -gx ATUIN_NOBIND "true"
-            atuin init fish | source
-            # gh fish source | source
-            gh completion -s fish > ~/.config/fish/completions/gh.fish
+        set -gx ATUIN_NOBIND "true"
+        atuin init fish | source
+        # gh fish source | source
+        gh completion -s fish > ~/.config/fish/completions/gh.fish
 
-            fzf --fish | FZF_CTRL_R_COMMAND= source
+        fzf --fish | FZF_CTRL_R_COMMAND= source
 
-            # vincular ao ctrl-r no modo normal e de inserção, adicione quaisquer outras vinculações aqui também
-            bind \cr _atuin_search
-            bind -M insert \cr _atuin_search
+        # vincular ao ctrl-r no modo normal e de inserção, adicione quaisquer outras vinculações aqui também
+        bind \cr _atuin_search
+        bind -M insert \cr _atuin_search
 
-            alias gnome-control-center="XDG_CURRENT_DESKTOP=Gnome /usr/bin/gnome-control-center"
-            alias ls="eza"
-            alias cd="z"
+        [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
 
-            # alias nvim-kickstart='NVIM_APPNAME="nvim-kickstart" nvim'
-            # alias nvim-lazyvim='NVIM_APPNAME="nvim-lazyvim" nvim'
-            # alias nvim-nv='NVIM_APPNAME="nvim-nv" nvim'
-            # alias nvim-astro='NVIM_APPNAME="nvim-astro" nvim'
+        set -x PATH $HOME/.nix-profile/bin $HOME/.nix-profile/sbin $PATH
+        set -x PATH $HOME/.config/herd-lite/bin $PATH
 
-            alias tmux-restore='tmux new -d -t temp && tmux run-shell ~/.tmux/plugins/tmux-resurrect/scripts/restore.sh && tmux kill-session -t temp && tmux attach'
-            alias fzftop='ps aux | fzf --multi --reverse --bind "ctrl-r:reload(ps aux)" --header="Press CTRL-R to reload" --header-lines=2 --preview="echo {}" --preview-window=down,3,wrap | awk "{print $2}" | xargs kill -9'
+        set -x COMPOSE_BAKE true
+        set -x TERMINAL /usr/bin/kitty
 
-            [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+        set -x GOOGLE_SEARCH_ENGINE_ID "956ead57766404115"
+        set -x GOOGLE_SEARCH_API_KEY "AIzaSyAMCWGpnuXzsBING56M_ryNuQWpI8d7G1Q"
 
-            set -x PATH $HOME/.nix-profile/bin $HOME/.nix-profile/sbin $PATH
-            set -x PATH $HOME/.config/herd-lite/bin $PATH
+        set -x TAVILY_API_KEY "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"
 
-            set -x COMPOSE_BAKE true
-            set -x TERMINAL /usr/bin/kitty
+        thefuck --alias | source
+        zoxide init fish | source
 
-            set -x GOOGLE_SEARCH_ENGINE_ID "956ead57766404115"
-            set -x GOOGLE_SEARCH_API_KEY "AIzaSyAMCWGpnuXzsBING56M_ryNuQWpI8d7G1Q"
-
-            set -x TAVILY_API_KEY "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"
-
-            thefuck --alias | source
-            zoxide init fish | source
-
-            function y
-              set tmp (mktemp -t "yazi-cwd.XXXXXX")
-              yazi $argv --cwd-file="$tmp"
-              if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-                builtin cd -- "$cwd"
-              end
-              rm -f -- "$tmp"
-            end
+        function y
+          set -l cwd_file (command cat -- "$argv[1]")
+          if test -n "$cwd_file"
+            builtin cd -- "$cwd_file"
+          else
+            echo "Erro: Caminho do diretório não encontrado no arquivo."
+          end
         end
+
+        function fzfpass
+          # Verificar se as dependências estão instaladas
+          if not command -v fzf >/dev/null
+            echo "Erro: fzf não está instalado"
+            return 1
+          end
+          
+          if not command -v fd >/dev/null
+            echo "Erro: fd não está instalado"
+            return 1
+          end
+          
+          if not command -v keepassxc-cli >/dev/null
+            echo "Erro: keepassxc-cli não está instalado"
+            return 1
+          end
+          
+          if not command -v wl-copy >/dev/null
+            echo "Erro: wl-clipboard não está instalado"
+            return 1
+          end
+          
+          # Selecionar banco de dados
+          set db_path (fd ".kdbx\$" $HOME | fzf --prompt="Selecione o banco de dados: ") 
+
+          if test -z "$db_path"
+            echo "Nenhum banco selecionado"
+            return 1
+          end
+          
+          # Ler senha
+          read -s -P "Digite a senha para KeePassXC: " keepasswd
+          
+          # Selecionar entrada e copiar senha
+          printf "%s\n" "$keepasswd" | keepassxc-cli ls "$db_path" | \
+          fzf --multi --reverse --preview="echo {}" --preview-window=down,3,wrap | \
+          while read -l entry
+            printf "%s\n" "$keepasswd" | keepassxc-cli show -q "$db_path" "$entry" -a Password | tr -d '\n' | wl-copy
+            echo "Senha copiada para clipboard: $entry"
+          end
+        end
+      end
     '';
   };
 
